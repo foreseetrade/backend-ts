f91d4e1235eb82b9042b930799ef63aaf0b673f0
Scriptscrypt <tirumaladasa@gmail.com>
2024-03-07 02:54:51 +0530

    Feat: Matches Routes

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,28 @@
-
-import express from 'express';
-import { json } from 'body-parser';
-import userRoutes from './routes/userRoutes';
-
+import express from "express";
+import { json } from "body-parser";
+import userRoutes from "./routes/userRoutes";
+import appInfoRoutes from "./routes/appinfoRoutes";
+import faqRoutes from "./routes/faqRoutes";
+import matchRoutes from "./routes/matchRoutes";
+import predictionRoutes from "./routes/predictionRoutes";
+import topupRoutes from "./routes/topupRoutes";
+import transactionRoutes from "./routes/transactionRoutes";
+import notificationRoutes from "./routes/notificationRoutes";
+import withdrawRoutes from "./routes/withdrawRoutes";
+import investmentsRoutes from "./routes/investmentRoutes";
 const app = express();
 
 app.use(json());
 
-app.use('/user', userRoutes);
+app.use("/app-info", appInfoRoutes);
+app.use("/faq", faqRoutes);
+app.use("/investment", investmentsRoutes);
+app.use("/match", matchRoutes);
+app.use("/notification", notificationRoutes);
+app.use("/prediction", predictionRoutes);
+app.use("/topup", topupRoutes);
+app.use("/transaction", transactionRoutes);
+app.use("/user", userRoutes);
+app.use("/withdraw", withdrawRoutes);
 
 export default app;
--- src/routes/matchRoutes.ts
+++ src/routes/matchRoutes.ts
@@ -1,29 +1,69 @@
 // matchRoutes.ts
-import express, { Request, Response } from 'express';
-import * as matchService from '../services/matchService';
+import express, { Request, Response } from "express";
+import * as matchService from "../services/matchService";
 
 const router = express.Router();
 
-router.get('/matches', async (req, res) => {
+router.get("/all", async (req, res) => {
   try {
     const matches = await matchService.getAllMatches();
+    console.log("Matches:", matches);
     res.status(200).json(matches);
   } catch (error) {
-    console.error('Error fetching matches:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error("Error fetching matches:", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
-router.get('/matches/:id', async (req, res) => {
+router.get("/:id", async (req, res) => {
   const matchId = parseInt(req.params.id, 10);
 
   try {
     const match = await matchService.getMatchById(matchId);
+    console.log("Match:", match);
+    if (!match) {
+      return res.status(404).json({ error: "Match not found" });
+    }
     res.status(200).json(match);
   } catch (error) {
-    console.error('Error fetching match by ID:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error("Error fetching match by ID:", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
+router.put("/update/:id", async (req, res) => {
+  const matchId = parseInt(req.params.id, 10);
+  const updatedMatchData = req.body;
+
+  try {
+    const updatedMatch = await matchService.updateMatch(
+      matchId,
+      updatedMatchData
+    );
+    res.status(200).json({
+      message: "Match updated successfully",
+      status: "success",
+      updatedMatch,
+    });
+  } catch (error) {
+    console.error("Error updating match:", error);
+    res.status(500).json({ message: "Internal Server Error", status: "error" });
+  }
+});
+
+router.post("/new", async (req, res) => {
+  const newMatchData = req.body;
+
+  try {
+    const newMatch = await matchService.createMatch(newMatchData);
+    res.status(201).json({
+      message: "Match created successfully",
+      status: "success",
+      newMatch,
+    });
+  } catch (error) {
+    console.error("Error creating match:", error);
+    res.status(500).json({ message: "Internal Server Error", status: "error" });
+  }
+});
 export default router;
